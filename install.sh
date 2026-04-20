fad3ddfe3d5ef41e5183821cddbfba9f78ee441c
2lazyyy <andreigherman230808@gmail.com>
2026-04-20 05:17:46 -0400

    changes

---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# INSTALLING APPS NEEDED
+
+#sudo apt update
+#sudo apt install -y firefox-esr bspwm sxhkd kitty fonts-dejavu-core git jq curl rofi feh gh ninja-build libmpdclient-dev libxcb-util0-dev xcb-proto libxcb-composite0-dev libpulse-dev libiw-dev libcurl4-openssl-dev libasound2-dev cmake build-essential python3-sphinx libuv1-dev libuv1 libx11-dev libxcb-randr0-dev libxcb-xinerama0-dev libxcb1-dev libxcb-util0-dev libx11-xcb-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-present-dev libxcb-sync-dev libxi-dev libxrandr-dev python3 python3-pip python3-setuptools python3-wheel python3-xcbgen
+#mkdir $HOME/.config/bspwm && mkdir $HOME/.config/sxhkd && mkdir $HOME/.config/buildsrc
+#git clone https://github.com/neovim/neovim $HOME/.config && cd $HOME/.config && git checkout stable && cd -
+
+# GIT CLONE WALLPAPER FOLDER INTO THE PICTURES FOLDER.
+#git clone https://github.com/2lazyyy/walls.git $HOME/Pictures
+
+# BUILDING NEOVIM FROM SOURCE
+
+#if [! -d "$HOME/.config/buildsrc/neovim"]; then
+#    git clone https://github.com/neovim/neovim "$HOME/.config/buildsrc/neovim"
+#fi
+#
+#cd "$HOME/.config/buildsrc/neovim" || exit
+#git checkout stable
+#
+#make CMAKE_BUILD_TYPE=RelWithDebInfo
+#sudo make install && cd "$HOME"
+#if [! -d "$HOME/.config/buildsrc/polybar"]; then
+#	git clone https://github.com/polybar/polybar.git "$HOME/.config/buildsrc/polybar"
+#fi
+
+#bash "$HOME/.config/buildsrc/polybar/build.sh"
+
+# REPLACE THIS LINE WITH MY ACTUAL GITHUB CONFIG.
+#cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd && cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm
+
+#chmod +x $HOME/.config/bspwm/bspwmrc && chmod +x $HOME/.config/sxhkd/sxhkdrc
+#git clone https://github.com/13-CF/afetch.git $HOME/.config && sudo make clean install -C $HOME/.config/afetch
+
+# INSTALLING NVIDIA DRIVERS: https://www.youtube.com/watch?v=QqINMp1UOnY
+
+#sudo mv -f $HOME/.config/dotfiles/install/configfiles/sources.list /etc/apt/sources.list
+#sudo apt modernize-sources
+#sudo apt update && sudo apt install linux-headers-$(dpkg --print-architecture)
+#sudo apt install nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree
+
+#CONFIGURING KEYBOARD BRIGHTNESS TO LAPTOP
+#(directories might be different because of your hardware)
+
+#echo "3" | sudo tee /sys/class/leds/asus::kbd_backlight/brightness > /dev/null
+
+#CONFIGURING SOFT TOUCH FOR LAPTOP
+
+#sudo mkdir -p /etc/X11/xorg.conf.d
+
+#sudo tee -a /etc/X11/xorg.conf.d/40-libinput.conf > /dev/null << 'EOF'
+#Section "InputClass"
+#    Identifier "libinput touchpad catchall"
+#    MatchIsTouchpad "on"
+#    MatchDevicePath "/dev/input/event*"
+#    Driver "libinput"
+#    Option "Tapping" "on"
+#    Option "TappingDrag" "on"
+#    Option "NaturalScrolling" "true"
+#EndSection
+#EOF
+
+# MAKES SO WHEN LAPTOP LID IS CLOSED THE LAPTOP KEEPS FUNCTIONING.
+
+#sudo cp /etc/systemd/logind.conf /etc/systemd/logind.conf.bak
+# Set HandleLidSwitch=ignore
+#sudo sed -i 's/^#\?HandleLidSwitch=.*/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+# Set HandleLidSwitchDocked=ignore
+#sudo sed -i 's/^#\?HandleLidSwitchDocked=.*/HandleLidSwitchDocked=ignore/' /etc/systemd/logind.conf
+#sudo systemctl restart systemd-logind
+
+
+
+
+
+
+
+
+
+
+# to-do list
+
+# troubleshoot slstatus
+# pull down the wallpapers, slstatus config, bspwm config and shhkd from my github.
+# configure kitty terminal.
+# configure rofi and picom (for transparent terminal).
+# configure zsh.
+# reboot
